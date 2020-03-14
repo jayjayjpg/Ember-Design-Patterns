49422af1d4c1599e5891a13f8fd777ea8c55cf12
Russell Snyder <russellsnyder@Russells-MBP.fritz.box>
2020-03-14 18:19:52 +0100

    refactor translation logic

--- app/pods/patterns/component/declarative-rendering/route.js
+++ app/pods/patterns/component/declarative-rendering/route.js
@@ -4,8 +4,6 @@ import { inject as service } from '@ember/service';
 const picsumURL = "https://picsum.photos/800/400?random=";
 const photos = [1,2,3,4,5,6,7,8].map(int => `${picsumURL}${int}`)
 
-const TRANSLATION_NAMESPACE = "patterns.declarative-rendering"
-
 export default Route.extend({
   intl: service(),
   beforeModel() {
@@ -15,29 +13,26 @@ export default Route.extend({
   },
 
   model() {
+    const t_ns = "patterns.declarative_rendering.";
+    const t_ns_pe = `${t_ns}pattern_explanation.`;
+
     return {
-      currentSlideIndex: 0,  
+      t_ns,
+      t_ns_pe,
       photos,
-      text: {
-        title: this.intl.t(`${TRANSLATION_NAMESPACE}.title`),
-        'pattern-explanation': {
-          show: this.intl.t(`${TRANSLATION_NAMESPACE}.pattern-explanation.show`),
-          hide: this.intl.t(`${TRANSLATION_NAMESPACE}.pattern-explanation.hide`),
-          overview: this.intl.t(`${TRANSLATION_NAMESPACE}.pattern-explanation.overview`),
-          'use-cases': [
-            this.intl.t(`${TRANSLATION_NAMESPACE}.pattern-explanation.use-cases.u1`),
-          ],
-          'rabbit-holes': [
-            this.intl.t(`${TRANSLATION_NAMESPACE}.pattern-explanation.rabbit-holes.r1`),
-          ],
-          'references': [
-            {
-              title: this.intl.t(`${TRANSLATION_NAMESPACE}.pattern-explanation.references.ref1.title`),
-              link: this.intl.t(`${TRANSLATION_NAMESPACE}.pattern-explanation.references.ref1.link`)
-            }
-          ]
-        }
-      }
+      currentSlideIndex: 0,
+      use_cases: [
+        this.intl.t(`${t_ns_pe}use_cases.u1`),
+      ],
+      rabbit_holes: [
+        this.intl.t(`${t_ns_pe}rabbit_holes.r1`),
+      ],
+      references: [
+        { 
+          title: this.intl.t(`${t_ns_pe}references.ref1.title`),
+          link: this.intl.t(`${t_ns_pe}references.ref1.link`)
+        },
+      ]
     }
   }
 });
